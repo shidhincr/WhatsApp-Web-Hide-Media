3f48f4bf76d4511a316f5ca28acecc3953e965fe
Shidhin <shidhincr@gmail.com>
2015-01-29 14:40:08 +0400

    Implemented content script, options page

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,29 @@
 'use strict';
+var opacity = 0.1;
+chrome.storage.sync.get('opacity', function(storage){
+	opacity = storage.opacity || 0.1;
+});
+var enableImageBindings = function(){
+	document.body.addEventListener(
+	    'load',
+	    function(event){
+	        var tgt = event.target;
+	        var $elem  = jQuery(tgt);
+	        if( $elem.is('img') && $elem.hasClass('image-thumb-body')){
+	            $elem.css({
+	            	visibility: 'hidden',
+	            	opacity: opacity
+	            });
+	            $elem.attr('data-ext-image-blocked', 'true');
+	            $elem.parent().hover(function(){
+	            	$elem.css('visibility', 'visible');
+	            },function(){
+	            	$elem.css('visibility', 'hidden');
+	            })
+	        }
+	    },
+	    true
+	);	
+};
 
-console.log('\'Allo \'Allo! Content script');
+enableImageBindings();
--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,3 +1,45 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Option');
+function showMessage(msg) {
+  var status = document.getElementById('status');
+  status.textContent = msg;
+  status.style.display = 'inline-block';
+  setTimeout(hideMessage, 1750);
+}
+
+function hideMessage() {
+  var status = document.getElementById('status');
+  status.textContent = '';
+  status.style.display = 'none';
+}
+
+function updateSettings() {
+  var opacity = parseFloat(document.getElementById('opacityValue').value, 10);
+
+  if (isNaN(opacity)) {
+    showMessage('Invalid transparency value');
+    return false;
+  }
+
+  if (opacity < 0 || opacity > 1) {
+    showMessage('Transparency value should be between 0 and 1');
+    return false;
+  }
+
+  chrome.storage.sync.set({
+    'opacity': opacity.toString()
+  }, function () {
+    // Update status to let user know options were saved.
+    showMessage('Settings saved successfully');
+  });
+
+  return false;
+}
+
+var updateBtn = document.getElementById('updateBtn');
+updateBtn.addEventListener('click', updateSettings);
+
+chrome.storage.sync.get('opacity', function(storage){
+	var opacity = storage.opacity;
+	document.getElementById('opacityValue').value = opacity;
+});
